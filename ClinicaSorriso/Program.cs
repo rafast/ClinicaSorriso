16fc6e340c0fabc7ac352c9462058df22fcc2777
Rafael Telles <rafast.telles@gmail.com>
2022-05-26 11:26:52 -0300

    Initial commit

---
+++ ClinicaSorriso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClinicaSorriso
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
