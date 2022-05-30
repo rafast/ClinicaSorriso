465f0656859568234bef24c3aa8dc143dadd6434
Rafael Telles <rafast.telles@gmail.com>
2022-05-30 17:45:34 -0300

    Add consulta service

---
+++ ClinicaSorriso/Services/ConsultaService.cs
@@ -0,0 +1,40 @@
+﻿using ClinicaSorriso.Models;
+using ClinicaSorriso.Repositories.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaSorriso.Services
+{
+    public class ConsultaService : IConsultaService
+    {
+        private ConsultaRepositoryInMemory _consultaRepositoryInMemory { get; set; }
+
+        public ConsultaService(ConsultaRepositoryInMemory consultaRepositoryInMemory)
+        {
+            _consultaRepositoryInMemory = consultaRepositoryInMemory;
+        }
+
+        public void CadastrarConsulta(Consulta consulta)
+        {
+            if (consulta.Paciente == null)
+            {
+                throw new ArgumentException("Consulta sem Paciente!!");
+            }
+            _consultaRepositoryInMemory.Salvar(consulta);
+
+        }
+
+        public void ExcluirConsulta(Consulta consulta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Consulta> ListarConsultas()
+        {
+            return _consultaRepositoryInMemory.ListarTodos().OrderBy(c => c.Data).ToList();
+        }
+    }
+}
---
+++ ClinicaSorriso/Services/IConsultaService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using ClinicaSorriso.Models;
+
+namespace ClinicaSorriso.Services
+{
+    public interface IConsultaService
+    {
+        List<Consulta> ListarConsultas();
+        void CadastrarConsulta(Consulta consulta);
+        void ExcluirConsulta(Consulta consulta);
+    }
+}
