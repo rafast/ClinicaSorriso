b60571c9e056436098b626408cf46fcedd8e05df
Rafael Telles <rafast.telles@gmail.com>
2022-05-30 17:39:56 -0300

    Add ConsultaRepositoryInMemory

---
+++ ClinicaSorriso/Repositories/InMemory/ConsultaRepositoryInMemory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using ClinicaSorriso.Models;
+
+
+namespace ClinicaSorriso.Repositories.InMemory
+{
+    public class ConsultaRepositoryInMemory : IRepository<Consulta>
+    {
+        private List<Consulta> _consultaRepository { get; set; }
+
+        public ConsultaRepositoryInMemory()
+        {
+            _consultaRepository = new List<Consulta>();
+        }
+
+        public void Salvar(Consulta entity)
+        {
+            _consultaRepository.Add(entity);
+        }
+
+        public void Deletar(Consulta entity)
+        {
+            _consultaRepository.Remove(entity);
+        }
+
+        public List<Consulta> ListarTodos()
+        {
+            return _consultaRepository;
+        }
+    }
+}
